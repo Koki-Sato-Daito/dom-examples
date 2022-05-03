be28b7715a649fbf5d8d168dd44dc8454cb35fde
Koki-Sato <s17161278@st.daito.ac.jp>
2022-05-04 03:28:50 +0900

    コメントの追加とイベントリスナーを作成

--- server-sent-events/sse.php
+++ server-sent-events/sse.php
@@ -1,17 +1,18 @@
 <?php
 date_default_timezone_set("America/New_York");
+// server sent eventsの場合Content-Typeは以下のようにする
 header("Content-Type: text/event-stream");
 
 $counter = rand(1, 10); // a random counter
 while (1) {
 // 1 is always true, so repeat the while loop forever (aka event-loop)
 
+  // クライアントに送信するメッセージを作成
   $curDate = date(DATE_ISO8601);
   echo "event: ping\n",
        'data: {"time": "' . $curDate . '"}', "\n\n";
 
   // Send a simple message at random intervals.
-
   $counter--;
 
   if (!$counter) {
@@ -20,18 +21,17 @@ while (1) {
   }
 
   // flush the output buffer and send echoed messages to the browser
-
+  // 出力用バッファネストレベルが0より大きい。(メッセージが作られていれば)
   while (ob_get_level() > 0) {
+    // 出力用バッファをフラッシュ(送信)
     ob_end_flush();
   }
+  // 送信
   flush();
 
   // break the loop if the client aborted the connection (closed the page)
-  
   if ( connection_aborted() ) break;
 
   // sleep for 1 second before running the loop again
-  
   sleep(1);
-
 }
